ae8f8c90d036a13a268b63b9cc80b504195be244
elcerdo <georges.wbushjr@laposte.net>
2011-06-14 18:44:36 +0200

    random generation

--- robot/generateRobot.py
+++ robot/generateRobot.py
@@ -4,6 +4,7 @@
 import cPickle
 import math
 import sys
+import random
 
 filename  = sys.argv[1]
 
@@ -11,11 +12,11 @@ data = {}
 data["motorRadius"] = 1.5
 data["mainLength"] = 10.
 data["mainHeight"] = 2.
-data["upperExtension"] = 3.
-data["legWidth"] = 3.
-data["legHeight"] = 5.
-data["legAngle"] = 20/180.*math.pi
-data["footHeight"] = 5.
-data["legNumber"] = 5
+data["upperExtension"] = random.randrange(.5,5,int=float)
+data["legWidth"] = random.randrange(1,4,int=float)
+data["legHeight"] = random.randrange(2,8,int=float)
+data["legAngle"] = random.random(-5,45,int=float)/180.*math.pi
+data["footHeight"] = random.randrange(2,8,int=float)
+data["legNumber"] = random.randrange(2,6)
 
 cPickle.dump(data,file(filename,"wb"))
