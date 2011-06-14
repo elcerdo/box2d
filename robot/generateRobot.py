bc34a154a08a665d16f8d23c769d027d598d3d8e
elcerdo <georges.wbushjr@laposte.net>
2011-06-14 18:52:39 +0200

    bugfix

--- robot/generateRobot.py
+++ robot/generateRobot.py
@@ -15,7 +15,7 @@ data["mainHeight"] = 2.
 data["upperExtension"] = random.randrange(.5,5,int=float)
 data["legWidth"] = random.randrange(1,4,int=float)
 data["legHeight"] = random.randrange(2,8,int=float)
-data["legAngle"] = random.random(-5,45,int=float)/180.*math.pi
+data["legAngle"] = random.randrange(-5,45,int=float)/180.*math.pi
 data["footHeight"] = random.randrange(2,8,int=float)
 data["legNumber"] = random.randrange(2,6)
 
