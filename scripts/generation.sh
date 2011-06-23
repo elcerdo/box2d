bea447d53e306af37fe95dce0c128f5649149c08
Pierre Gueth <gueth@linux36.dg.creatis.insa-lyon.fr>
2011-06-23 14:28:10 +0200

    generation script

--- scripts/generation.sh
+++ scripts/generation.sh
@@ -2,16 +2,34 @@
 
 set -u
 
-kkmax=100
+kkmax=1000
+
+/usr/bin/which simulateRobot > /dev/null 2> /dev/null || exit 4
+
+scratch="$(mktemp -d "run.XXXX")"
+echo "scratch is ${scratch}"
+
 failed=0
-
-rm -f "definition.*.pck"
-rm -f "performance.*.pck"
-
+success=0
 for kk in $(seq ${kkmax}); do
-    echo "robot ${kk}/${kkmax} ${failed}"
-    definition_file="definition.${kk}.pck"
-    performance_file="performance.${kk}.pck"
-    generateRobot.py "${definition_file}"
-    simulateRobot "${definition_file}" "${performance_file}" || let "failed++" && continue
+    echo "robot ${kk}/${kkmax} ${success} ${failed}"
+    definition_file="${scratch}/definition.${kk}.pck"
+    performance_file="${scratch}/performance.${kk}.pck"
+    generateRobot.py "${definition_file}" > /dev/null
+    simulateRobot "${definition_file}" "${performance_file}" > /dev/null 2> /dev/null
+
+    if test $? -ne 0
+    then
+        echo "failed"
+        let "failed++"
+        rm "${definition_file}" "${performance_file}"
+        continue
+    fi
+
+    let "success++"
+    echo "success"
+
+    test ${success} -ge 50 && break
 done
+
+echo "kk=${kk} success=${success} failed=${failed}"
